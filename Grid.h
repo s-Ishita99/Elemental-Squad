532979e7ff40e499421a4e8cbf0b4883810d196e
s-Ishita99 <singhishita119@gmail.com>
2026-09-23 19:22:11 +0530

    Add Grid class with initialization and access methods

---
+++ Grid.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Grid
+{
+private:
+    static const int ROWS = 10;
+    static const int COLS = 14;
+
+    int grid[ROWS][COLS];
+
+public:
+    Grid();
+
+    void initialize();
+    int getTile(int row, int col) const;
+};
