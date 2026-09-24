80f3767f522eea5ce87e3d591d5736160c599af3
s-Ishita99 <singhishita119@gmail.com>
2026-09-25 02:53:40 +0530

    Add position attributes and movement methods to Character

--- Character.cpp
+++ Character.cpp
@@ -1,15 +1,48 @@
 #include "Character.h"
 
-Character::Character(string n, string a, string t)
+Character::Character(string n, string a, string t, int startX, int startY)
 {
     name = n;
     ability = a;
     task = t;
+
+    x = startX;
+    y = startY;
 }
 
 string Character::getName()
 {
     return name;
+}
+
+int Character::getX()
+{
+    return x;
+}
+
+int Character::getY()
+{
+    return y;
+}
+
+void Character::moveUp()
+{
+    y--;
+}
+
+void Character::moveDown()
+{
+    y++;
+}
+
+void Character::moveLeft()
+{
+    x--;
+}
+
+void Character::moveRight()
+{
+    x++;
 }
 
 Character::~Character()
